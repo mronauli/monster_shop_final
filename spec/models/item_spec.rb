4e12c56f20e28f5ec3f07312efb9d8598022f044
Maria Ronauli <mronauli@gmail.com>
2020-03-03 19:27:38 -0700

    add tests for item method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -158,5 +158,26 @@ describe Item, type: :model do
       expect(Item.bottom_5_items[3]).to eq(pull_toy)
       expect(Item.bottom_5_items[4]).to eq(dog_bone)
     end
+    describe "discounts?" do
+      before(:each) do
+        @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+        @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+        @discount_1 = Discount.create(name: "Black Friday", percentage: 0.2, bulk: 20, merchant: @meg)
+        @discount_2 = Discount.create(name: "10% Off", percentage: 0.1, bulk: 10, merchant: @meg)
+        @discount_3 = Discount.create(name: "Winter Sale", percentage: 0.1, bulk: 10, merchant: @bike_shop)
+        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 500)
+        @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 50)
+        @pump = @bike_shop.items.create(name: "Pump", description: "for filling flat tires", price: 25, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 95)
+        @items = [@tire, @chain, @pump]
+        @contents = Hash.new(0)
+        @contents[@tire.id] = 20
+        @contents[@chain.id] = 34
+        @contents[@pump.id] = 9
+        @cart = Cart.new(@contents)
+      end
+      it "can check if any discounts can be applied to bulk item amount in cart" do
+        expect(@tire.any_discounts?(@contents[@tire.id])).to eq(@discount_1)
+      end
+    end
   end
 end
